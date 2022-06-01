3607765069b22e6c66f492eb8c24c0bb9c5558c6
sajib-hossan1 <sajib.hossan112@gmail.com>
2022-06-02 05:46:18 +0600

    any data type

---
+++ user-defined-data-type/any-type.js
@@ -0,0 +1,8 @@
+var everything;
+everything = "anis";
+everything = 1;
+everything = true;
+everything = ["anis", "bhai"];
+everything = [1, 2];
+everything = [1, "2"];
+everything = { id: 1, name: "anis" };
---
+++ user-defined-data-type/any-type.ts
@@ -0,0 +1,9 @@
+let everything : any;
+
+everything = "anis";
+everything = 1;
+everything = true;
+everything = ["anis", "bhai"];
+everything = [1, 2];
+everything = [1, "2"];
+everything = {id :1, name : "anis"};
